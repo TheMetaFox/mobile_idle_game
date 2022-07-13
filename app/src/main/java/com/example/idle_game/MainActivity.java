114753cfa864567ed17f03dd1555b1d70724ee6b
Joshua Peters <petersjoshuaedu@gmail.com>
2022-07-13 00:25:00 -0400

    initial incrementation

--- app/src/main/java/com/example/idle_game/MainActivity.java
+++ app/src/main/java/com/example/idle_game/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.idle_game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button GenerateStuffButton;
+    TextView StuffAmountTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GenerateStuffButton = findViewById(R.id.generate_stuff_button);
+        StuffAmountTextView = findViewById(R.id.stuff_amount_textview);
+
+        GenerateStuffButton.setOnClickListener(v -> {
+            int stuffAmount = (int) StuffAmountTextView.getAlpha();
+            stuffAmount++;
+            StuffAmountTextView.setText(stuffAmount);
+        });
     }
 }
