bbc5d9df13b3ae6e8ce91b8e1080874f91420dfc
Joshua Peters <petersjoshuaedu@gmail.com>
2022-08-15 11:20:55 -0400

    Generation Pauses with Exit

--- app/src/main/java/com/example/idle_game/MainActivity.java
+++ app/src/main/java/com/example/idle_game/MainActivity.java
@@ -1,12 +1,27 @@
 package com.example.idle_game;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "ActivityState";
+    LocalDateTime closeDateTime;
+    LocalDateTime openDateTime;
+
+    boolean activityVisible;
+    boolean threadActive = false;
 
     DBHelper DBHelper;
 
@@ -17,9 +32,11 @@ public class MainActivity extends AppCompatActivity {
     TextView FirstGenerationLevelTextView;
 
     Runnable runnable = new Runnable() {
+        @RequiresApi(api = Build.VERSION_CODES.O)
         @Override
         public void run() {
-            while(true) {
+            while(activityVisible) {
+                threadActive = true;
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
@@ -33,8 +50,12 @@ public class MainActivity extends AppCompatActivity {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
+                    break;
                 }
             }
+            threadActive = false;
+            Log.d(TAG, String.valueOf(Thread.activeCount()));
+            //Log.i(TAG, String.valueOf(openDateTime.minus(closeDateTime.getHour(), ChronoUnit.HOURS)));
         }
     };
 
@@ -42,6 +63,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate");
+
 
         DBHelper = new DBHelper(MainActivity.this);
 
@@ -51,10 +74,8 @@ public class MainActivity extends AppCompatActivity {
         StuffAmountTextView = findViewById(R.id.stuff_amount_textview);
         FirstGenerationLevelTextView = findViewById(R.id.first_generation_level_textview);
 
-        //TimerThread Thread = new TimerThread(MainActivity.this);
-        //Thread.start();
-        Thread thread = new Thread(runnable);
-        thread.start();
+        FirstGenerationLevelTextView.setText(String.valueOf(DBHelper.getLevel(1)));
+
 
         GenerateStuffButton.setOnClickListener(v -> {
             DBHelper.updateStuff(1);
@@ -62,8 +83,20 @@ public class MainActivity extends AppCompatActivity {
         });
 
         FirstGenerationButton.setOnClickListener(v -> {
+            if (DBHelper.getStuff() < 10) {
+                Context context = getApplicationContext();
+                CharSequence text = "Not Enough Stuff!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+
             DBHelper.levelUp(1);
+            DBHelper.updateStuff(-10);
             FirstGenerationLevelTextView.setText(String.valueOf(DBHelper.getLevel(1)));
+            StuffAmountTextView.setText(String.valueOf(DBHelper.getStuff()));
+
         });
 
         ResetStatsButton.setOnClickListener(v -> {
@@ -72,5 +105,59 @@ public class MainActivity extends AppCompatActivity {
             FirstGenerationLevelTextView.setText("0");
         });
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i(TAG, "onStart");
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        activityVisible = true;
+        openDateTime = LocalDateTime.now();
+
+        if (!threadActive) {
+            Thread thread = new Thread(runnable);
+            thread.start();
+        }
+
+        Log.i(TAG, "onResume :: activityVisible = true");
+    }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        activityVisible = false;
+        closeDateTime = LocalDateTime.now();
+
+        Log.i(TAG, "onStop :: activityVisible = false");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.i(TAG, "onRestart");
     }
 }
